740672566afa6d04e514ed2a6d5c8b488f881adc
Austin Krzciok <38452989+Kazooka503@users.noreply.github.com>
2020-05-09 03:21:18 -0700

    Basic JavaScript Assignment
    Assignment utilizing window.alert() and document.write(), assigning string values to variables and concatenating them together, assigning a color to variables using fontcolor() and writing an expression

---
+++ js/basic_javascript_1.js
@@ -0,0 +1,9 @@
+window.alert("ALERT!");
+
+var textone = "JavaScript is";
+var textone = textone.fontcolor("orange");
+var texttwo = " pretty awesome!";
+var texttwo = texttwo.fontcolor("green");
+document.write(textone + texttwo);
+
+
