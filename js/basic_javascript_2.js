cf6c751ea45e14889407c2658e95d87976866798
Austin Krzciok <38452989+Kazooka503@users.noreply.github.com>
2020-05-09 20:53:00 -0700

    Basic JavaScript Assignment 2
    Assignment with function utilizing += operator

---
+++ js/basic_javascript_2.js
@@ -0,0 +1,11 @@
+function demo() {
+    var a = "Text Here for demo!";
+    var result = a.fontcolor("red");
+    document.getElementById("demo-text").innerHTML = result;
+}
+
+function demoTwo() {
+    var x = "Portland";
+    x += " Timbers";
+    document.getElementById("con").innerHTML = x;
+}
